80e480c2676e536e91d66c7bef9407318bab7a4a
Diontae <diontaelow@gmail.com>
2023-12-02 00:22:25 +0800

    Completion of welcome-section of Home page

---
+++ home.js
@@ -0,0 +1,6 @@
+// JavaScript to add the class to trigger the animation
+document.addEventListener("DOMContentLoaded", function() {
+    const welcomeSection = document.querySelector(".welcome-section");
+    welcomeSection.classList.add("show-text");
+  });
+  
