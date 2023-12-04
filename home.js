6309b9ff78e22c73c5928ec107cbda232332977a
Diontae <diontaelow@gmail.com>
2023-12-05 02:02:29 +0800

    Completed JavaScript for Home page

--- home.js
+++ home.js
@@ -4,3 +4,27 @@ document.addEventListener("DOMContentLoaded", function() {
     welcomeSection.classList.add("show-text");
   });
   
+const boat = document.getElementById('boat');
+const lightning = document.getElementById('lightning');
+
+const flashLightning = () => {
+  // Show the lightning
+  lightning.style.visibility = 'visible';
+
+  // Hide the lightning after a short delay
+  setTimeout(() => {
+    lightning.style.visibility = 'hidden';
+  }, 200); // Adjust the duration of the flash as needed
+
+  // Repeat the flashing effect after a delay
+  setTimeout(flashLightning, 2000); // Adjust the interval between flashes as needed
+};
+
+// Trigger the flashing animation
+flashLightning();
+
+window.addEventListener('scroll', function() {
+  var scrollPosition = window.scrollY;
+  var boatElement = document.getElementById('boat');
+  boatElement.style.top = scrollPosition + 'px';
+});
